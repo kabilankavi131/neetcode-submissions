05be09e74ccbb05125eb01189b06523ddcbd34fa
Kabilan K <kabilankavi131@gmail.com>
2026-08-23 20:22:20 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> unique =new HashSet<>();
+        for(int i:nums){
+            if(!(unique.add(i))){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
